d011a9fb57760ba527f93557c5dc738c327bd978
Varen Aggarwal <varenaggarwal@gmail.com>
2023-02-27 12:40:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Stopwatch from "./components/Stopwatch";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Practice</h1>
+      <h2>StopWatch</h2>
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Stopwatch.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [isRunning]);
+
+  const toggleStartStop = () => {
+    setIsRunning((isRunning) => !isRunning);
+  };
+  const handleReset = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+  return (
+    <div>
+      <p>Time: {time} sec</p>
+      {console.log({ time, isRunning })}
+      <button onClick={toggleStartStop}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
+
+export default Stopwatch;
